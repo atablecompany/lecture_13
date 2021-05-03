9f7f0d4d239e97ec0141f4afe4fb3608e5edccbc
atablecompany <tenglados@gmail.com>
2021-05-03 10:18:02 +0200

    hotovo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
 
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        output = recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+        return output
+
+
 def main():
-    pass
+    print(recursive_nth_fibo(n=7))
 
 
 if __name__ == '__main__':
